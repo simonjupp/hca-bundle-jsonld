9b993eaa6ad380efc35e96270e61d2025f8ce9b3
Simon Jupp <simon.jupp@gmail.com>
2018-03-22 14:27:54 +0900

    changed links to create a direct triple

--- bundle2rdf.py
+++ bundle2rdf.py
@@ -13,6 +13,7 @@ from rdflib.plugin import register, Parser
 register('application/ld+json', Parser, 'rdflib_jsonld.parser', 'JsonLDParser')
 
 context = {
+    "@base": "http://rdf.data.humancellatlas.org/",
     "@vocab" : "http://rdf.data.humancellatlas.org/",
     "rdf" : "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
     "NCBITaxon" : "http://purl.obolibrary.org/obo/NCBITaxon_",
@@ -24,15 +25,12 @@ context = {
       "@type": "@id",
       "@id": "rdf:type"
     },
-    "document_id" : {
-      "@type" : "@vocab"
-     },
-    "source_id" : {
-      "@type" : "@vocab"
-     },
+    "document_id" :  "@id",
+    "source_id" : "@id",
     "destination_id" : {
-      "@type" : "@vocab"
-     }
+      "@type" : "@id",
+      "@id" : "links"
+    }
   }
 
 def getBundleManifest(submissionEnvelope):
